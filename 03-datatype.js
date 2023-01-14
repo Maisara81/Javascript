1a688134671d0a26110b39292c05ae00b344f21e
Maisara <maisarashafiq01@gmail.com>
2023-01-13 20:21:44 -0700

    Completed datatype

---
+++ 03-datatype.js
@@ -0,0 +1,35 @@
+// datatype/
+
+/*
+1. number
+2. string
+3. object
+*/
+
+// number 
+
+var x = 10
+console.log("x : ",x)
+
+var y = typeof(x)
+console.log("datatype : ",y)
+
+// string
+
+var name = "Maisara"
+console.log("name : ",name)
+
+var w = typeof(name)
+console.log("datatype : ",w)
+
+// object
+
+var obj = {
+    name : "Maisara",
+    age : 15,
+    country : "Canada",
+}
+
+console.log("obj : ", obj)
+
+console.log(typeof(obj))
