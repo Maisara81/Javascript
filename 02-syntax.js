05d38e69d366e9bc11a7eb6f07fab06bb1fba28a
Maisara <maisarashafiq01@gmail.com>
2023-01-13 20:08:28 -0700

    completed variables and syntax

---
+++ 02-syntax.js
@@ -0,0 +1,17 @@
+// javascript syntax /
+
+/* multi-line comment
+var a = 4
+console.log(a)
+*/
+
+//indentation/
+ if (true){
+    console.log("intent")
+}
+
+var firstNumber = 5
+var secondNumber = 6
+var totalNumber = firstNumber + secondNumber
+
+console.log(totalNumber)
--- hello.js
+++
@@ -1,2 +0,0 @@
-console.log("java")
-//alert("music")//
